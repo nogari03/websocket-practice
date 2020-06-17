4fb95efaa14075aabebaa875e86cf7218ced30e8
nogari03 <mmgg555@naver.com>
2020-06-17 16:00:28 +0900

    update redis

--- src/main/java/com/gangchat/Application.java
+++ src/main/java/com/gangchat/Application.java
@@ -1,4 +1,4 @@
-package com.gangchat.chat;
+package com.gangchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/gangchat/chat/dto/ChatRoom.java
+++ src/main/java/com/gangchat/chat/dto/ChatRoom.java
@@ -3,11 +3,14 @@ package com.gangchat.chat.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Getter
 @Setter
-public class ChatRoom {
+public class ChatRoom implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006639L;
     private String roomId;
     private String name;
 
@@ -18,5 +21,3 @@ public class ChatRoom {
         return chatRoom;
     }
 }
-
-
