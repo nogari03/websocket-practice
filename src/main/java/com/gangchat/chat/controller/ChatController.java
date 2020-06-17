0c03fe0cfa92f994039bdbc6210d08596cc210ff
nogari03 <mmgg555@naver.com>
2020-06-17 14:01:08 +0900

    update room,room detail

--- src/main/java/com/gangchat/chat/controller/ChatController.java
+++ src/main/java/com/gangchat/chat/controller/ChatController.java
@@ -1,25 +1,21 @@
 package com.gangchat.chat.controller;
 
-import com.gangchat.chat.dto.ChatRoom;
-import com.gangchat.chat.service.ChatService;
+import com.gangchat.chat.dto.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class ChatController {
-    private final ChatService chatService;
 
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatService.createRoom(name);
-    }
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.JOIN.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
 }
